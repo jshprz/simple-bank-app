970a7c6cd732b9d8c874236d01c440c42c755a74
Joshua Perez <joshua@smedia.com.au>
2023-03-16 03:20:31 +0800

    initial commit

---
+++ simple-bank-app/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ObjectOrientedCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Josh", 10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
+
+            account.MakeWithdrawal(120, DateTime.Now, "Hammock");
+            account.MakeWithdrawal(50, DateTime.Now, "Xbox Game");
+
+
+            Console.WriteLine(account.GetAccountHistory());
+
+
+            // Test for a negative balance:
+            try
+            {
+                account.MakeWithdrawal(100, DateTime.Now, "Attempt to overdraw");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
+
+            // Test that the initial balances must be positive.
+            try
+            {
+                var invalidAccount = new BankAccount("Invalid", 55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
